1e6a40b5c777c1d2926b39459bc24c9bb28e9100
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-05-05 00:20:30 -0400

    commit tree 9e1180c43d37ed7976ff6f9756481d3718015d5b

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import ArticleList from "./ArticleList"
+
+function Article({title, date = "January 1, 1970", preview}) {
+    //console.log(post)
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
+        </article>
+    )
+}
+
+export default Article;
